d98604d7cfc88d589b5c68aa030047d7836860f8
sonya.wong <sonya.wong@lc.cuny.edu>
2023-02-10 17:10:33 -0500

    update

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Variables:
+    2D array: 6 rows, 7 columns
+        0 = nobody put their piece
+        1 = player 1
+        2 = player 2
+    
+Logic for determining win:
+    1 for loop to check rows with a nested or loop to check columns
+*/
+
