5a6e86d05eeda60bf8723dc03113a903cbbd39fe
Sonya Wong <qweenii@Sonyas-Air.home>
2023-02-11 20:54:29 -0500

    update

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,101 +1,145 @@
-﻿/*
-Variables:
-    2D array: 6 rows, 7 columns
-        0 = nobody put their piece
-        1 = player 1
-        2 = player 2
-    
-Logic for determining win:
-    1 for loop to check rows with a nested or loop to check columns
-*/
-using System;
+﻿using System;
+using System.Linq;
+using System.Text;
 
 public class Connect4 {
     public static void Main(string[] args) {
 
-        int[,] tttGrid = new int[3, 3];
-        // foreach(int box in tttGrid.Length){
-        // Array.Fill(tttGrid[box], 0);
-        // }
-        const int player1 = 1;
-        const int player2 = 2;
-        int currentPlayer = player1;
-
-    // Length is the total length of the whole array
-        // int gridRow = tttGrid.Length;
-        // int gridCol = tttGrid.GetLength(0);
-
-        // Console.WriteLine("Row {0} , Col {1}", gridRow, gridCol);
-
-        Console.WriteLine(@"
-             |     |    
-          -  |  -  |  -
-        _____|_____|_____
-             |     |    
-          -  |  -  |  -
-        _____|_____|_____              
-             |     |    
-          -  |  -  |  -
-             |     |
-        ");
+        int[,] tttGrid = new int[7, 7];
+        int[] colProgress = new int[tttGrid.GetLength(0)];
+        Array.Fill(colProgress, tttGrid.GetLength(0)-1);
 
         int turn = tttGrid.Length;
         int currPlayer = 1;
+        int numberToWin = 4;
+        int player1 = 1;
+        int player2 = 2;
         
-        Console.WriteLine(turn);
 //*
         while(turn != 0) {
-            
-            
-            Console.Write("Player {0}'s turn:\nRow:", currPlayer);
-            int playerRow = int.Parse(Console.ReadLine()) - 1;
-            Console.Write("Column:");
-            int playerCol = int.Parse(Console.ReadLine()) - 1;
 
+            drawGrid(tttGrid);
             
-            if(playerRow > 2 || playerCol > 2 || playerRow < 0 || playerCol < 0){
-                Console.WriteLine("Invalid Row/Column. Please input a number 1 through 3");
+        /*  Console.ForegroundColor = (currPlayer == 1) ? ConsoleColor.Red : ConsoleColor.Blue;
+            Console.Write("Player {0}'s turn:\nRow: ", currPlayer);
+            Console.ForegroundColor = ConsoleColor.White;
+            int playerRow = int.Parse(Console.ReadLine()) - 1;
+        //*/
+
+        //* PROMPTING USER FOR COLUMN INPUT
+            Console.ForegroundColor = (currPlayer == player1) ? ConsoleColor.Red : ConsoleColor.Blue;
+            Console.Write("Column: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            int playerCol = 0;
+            bool playerInput = int.TryParse(Console.ReadLine(), out playerCol);
+            playerCol--;
+            Console.WriteLine(playerCol);
+        //*/
+
+        // IF PLAYER DID NOT INPUT A VALUE OR A VALID COLUMN, PROMPT PLAYER TO TRY AGAIN
+            if(playerCol > tttGrid.GetLength(0)-1 || playerCol < 0 || !playerInput){
+                Console.WriteLine("Invalid Column. Please input a number 1 through 3\n");
                 continue;
             }
-            if(tttGrid[playerRow, playerCol] == 0) {
-                
+        // IF COLUMN IS FULL PROMPT PLAYER TO TRY AGAIN
+            if(colProgress[playerCol] < 0) {
+                Console.WriteLine("That column is full. Choose another column.\n");
+                continue;
             }
-            int val = tttGrid[playerRow, playerCol];
+        // IF ABLE TO, PLACE PIECE IN DESIRED COLUMN, AND CHECK WIN
+            if(tttGrid[colProgress[playerCol], playerCol] == 0) {
+                tttGrid[colProgress[playerCol], playerCol] = currPlayer;
+                colProgress[playerCol]--;
+                bool playerWon = checkWin(tttGrid, currPlayer, numberToWin); // REMEMBER to adjust numToWin
+                if(playerWon) {
+                    drawGrid(tttGrid);
+                    Console.WriteLine("Player {0} won!!", currPlayer);
+                    break;
+                }
+            }   
 
-            Console.WriteLine("Box taken? {0}", val);
-            // if(tttGrid[playerRow-1, playerCol-1] != 0) {
-            //     Console.WriteLine("Somebody already went there! Go again.");
-            //     continue;
-            // }
-
-            currPlayer = (currPlayer == 1) ? 2 : 1;
-            turn--;
+        // ALTERNATE BETWEEN PLAYER 1 AND 2
+            currPlayer = (currPlayer == player1) ? player2 : player1;
+        // LIMITS HOW MANY TIMES PLAYERS CAN GO ACCUMULATIVELY
+            turn--; 
         }
 //*/
     }
 
+    public static void drawGrid(int[,] grid) {
+        for(int i = 0; i < grid.GetLength(0); i++) {
+            for(int k = 0; k < grid.GetLength(0); k++) {
+                Console.Write("     ");
+                if(k != grid.GetLength(0) - 1) Console.Write("|");
+            }
+            Console.WriteLine();
+            for(int j = 0; j < grid.GetLength(0); j++) {
+                int val = grid[i, j];
+                switch (val)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("  -  ");
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("  O  ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("  O  ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;                        
+                    default: 
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+                if(j != grid.GetLength(0) - 1) Console.Write("|");
+            }
+            Console.WriteLine();
+            for(int k = 0; k < grid.GetLength(0); k++) {
+                Console.Write("     ");
+                if(k != grid.GetLength(0) - 1) Console.Write("|");
+            }
+            Console.WriteLine();
+            if(i != grid.GetLength(0) - 1){
+                for(int k = 0; k < grid.GetLength(0)+1; k++) {
+                    Console.Write("-----");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
 
+    private static bool checkWin(int[,] grid, int player, int numToWin) {
 
-    private bool checkWin(int[,] grid, int player) {
-        int top = 0;
-        int left = 0;
-        int right = 0;
-        int bot = 0;
-        int midH = 0;
-        int midV = 0;
-        int diagL = 0;
-        int diagR = 0;
-        for(int i = 0; i < grid.Length; i++) {
-            if(grid[0, i] == player) top++;
-            if(grid[1, i] == player) midH++;
-            if(grid[2, i] == player) bot++;
-            if(grid[i, 0] == player) left++;
-            if(grid[i, 1] == player) midV++;
-            if(grid[i, 2] == player) right++;
-            if(grid[i, i] == player) diagL++;
-            if(grid[i, 2-i] == player) diagR++;
+        int[] winV = new int[grid.GetLength(0)]; // keeps track of vertical win
+        for(int i = grid.GetLength(0)-1; i >= 0 ; i--) {
+            int winH = 0; // keeps track of horizontal win
+            for(int j = 0; j < grid.GetLength(0); j++) {
+                if(grid[i, j] == player) {
+                    winH ++;
+                    winV[j]++;
+                }
+                else {
+                    winH = 0;
+                    winV[j] = 0;
+                }
+                if(winH == numToWin || winV[j] == numToWin) {
+
+                    if(player == 1) {
+                        Console.Write("Vertical win:");
+                        for(int t = 0; t < winV.Length; t++) {
+                            Console.Write(" {0}", winV[t]);
+                        } Console.WriteLine();
+                    }
+                    return true;
+                }
+            }
         }
 
-        return (top == 3 || left == 3 || right == 3 || bot == 3 || midH == 3 || midV == 3 || diagL == 3 || diagR == 3);
+        return false;
+
     }
 }
