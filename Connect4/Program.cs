0f3465ae1868a994779a98830a358601a568cf31
Meet Patel <meetpatel3299@icloud.com>
2023-02-12 11:51:26 -0600

    winning conditions and draw condition

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -4,15 +4,25 @@ namespace TicTacToe
 {
     class MainMenu
     {
-        static char[] arr = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static char[] arr = {'o', 'x', 'o', 'x', 'o', 'o', 'x', 'o', 'x'};
 
         public static void Main(string[] args)
         {
             Console.WriteLine("TicTacToe Game Start");
             TTOBoard();
+            bool winning = false;
+            if(WinningCondition() == true) {winning = true;}
+            Console.WriteLine("Winning Check: "+ winning);
+            bool draw = false;
+            if(WinningCondition() == false) 
+            {
+                if(DrawCondition() == true){draw = true;}
+                else {draw = false;}
+            }
+            Console.WriteLine("Draw Check: " + draw);
         }
 
-        private static void TTOBoard()
+        public static void TTOBoard()
         {
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
@@ -23,6 +33,40 @@ namespace TicTacToe
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
             Console.WriteLine("     |     |      ");
+        }
+
+        public static bool WinningCondition()
+        {
+            // Row Winning Statements
+            if (arr[0] == arr[1] && arr[1] == arr[2])      {return true;}
+            else if (arr[3] == arr[4] && arr[4] == arr[5]) {return true;}
+            else if (arr[6] == arr[7] && arr[7] == arr[8]) {return true;}
+
+            // Col Winning Statements
+            else if (arr[0] == arr[3] && arr[3] == arr[6]) {return true;}
+            else if (arr[1] == arr[4] && arr[4] == arr[7]) {return true;}
+            else if (arr[2] == arr[5] && arr[5] == arr[8]) {return true;}
+
+            //Diagonal Winning Statements
+            else if (arr[0] == arr[4] && arr[4] == arr[8]) {return true;}
+            else if (arr[2] == arr[4] && arr[4] == arr[6]) {return true;}
+
+            // No true winning conditions then return false
+            else {return false;}
+        }
+
+        public static bool DrawCondition()
+        {
+            // loop the array of characters
+            for(int i = 0; i < arr.Length; i++)
+            {
+                // if character is a character or 0.1.2.3.4.5.6.7.8. then return no draw false
+                if(arr[i] == '0' || arr[i] == '1' || arr[i] == '2' ||
+                    arr[i] == '3' || arr[i] == '4' || arr[i] == '5' ||
+                    arr[i] == '6' || arr[i] == '7' || arr[i] == '8') {return false;}
+            }
+            // return true all is filled with x and o 
+            return true;
         }
     }
 }
