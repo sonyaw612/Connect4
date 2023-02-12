1cb3bae78f71e6fad646daf6f1650a4001264210
Meet Patel <meetpatel3299@icloud.com>
2023-02-12 12:51:14 -0600

    tictactoe game working

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -4,22 +4,92 @@ namespace TicTacToe
 {
     class MainMenu
     {
-        static char[] arr = {'o', 'x', 'o', 'x', 'o', 'o', 'x', 'o', 'x'};
+        static char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+        static int playerTurn = 1; 
+        static int choosefield; 
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("TicTacToe Game Start");
+            Console.WriteLine("TicTacToe Game");
             TTOBoard();
-            bool winning = false;
-            if(WinningCondition() == true) {winning = true;}
-            Console.WriteLine("Winning Check: "+ winning);
-            bool draw = false;
-            if(WinningCondition() == false) 
-            {
-                if(DrawCondition() == true){draw = true;}
-                else {draw = false;}
+            //bool winning = false;
+            //if(WinningCondition() == true) {winning = true;}
+           // Console.WriteLine("Winning Check: "+ winning);
+           // bool draw = false;
+           // if(WinningCondition() == false) 
+           // {
+           //     if(DrawCondition() == true){draw = true;}
+           //     else {draw = false;}
+           // }
+           // Console.WriteLine("Draw Check: " + draw);
+            while(WinningCondition() != true){
+                Console.WriteLine("\n");
+                if(playerTurn == 1) 
+                {
+                    Console.WriteLine("Player 1 Turn");
+                    Console.WriteLine("Player1:X");
+                }
+                else if(playerTurn == 2) 
+                {
+                    Console.WriteLine("Player 2 Turn");
+                    Console.WriteLine("Player2:O");
+                }
+                Console.WriteLine("\n");
+
+                TTOBoard();
+
+                Console.WriteLine("Choose A Field Number: ");
+                choosefield = int.Parse(Console.ReadLine());
+
+                if (arr[choosefield] != 'X' && arr[choosefield] != 'O')
+                {
+                    if (playerTurn == 1) 
+                    {
+                        arr[choosefield] = 'X';
+                        playerTurn++;
+                    }
+                    else if(playerTurn == 2)
+                    {
+                        arr[choosefield] = 'O';
+                        playerTurn--;
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Field Already Taken Try Again");
+                    Console.WriteLine("\n");
+                }
+                TTOBoard();
+                bool winning = false;
+                if(WinningCondition() == true) {winning = true;}
+                Console.WriteLine("Winning Check: " + winning);
+                bool draw = false;
+                if(WinningCondition() == false) 
+                {
+                    if(DrawCondition() == true)
+                    {
+                        draw = true;
+                        Console.WriteLine("Draw Check: " + draw);
+                        break;
+                    }
+                    else {draw = false;}
+                }
+                Console.WriteLine("Draw Check: " + draw);
             }
-            Console.WriteLine("Draw Check: " + draw);
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            TTOBoard();
+            Console.WriteLine("\n");
+            if(WinningCondition() == true)
+            {
+                if(playerTurn == 1) {Console.WriteLine("Player 2 Has Won");}
+                else if(playerTurn == 2) {Console.WriteLine("Player 1 Has Won");}
+            }
+            else if(DrawCondition() == true)
+            {
+                Console.WriteLine("Game : Draw");
+            }
+
         }
 
         public static void TTOBoard()
